67f720fc6cfeea2b576dd6d19841e3afafd250ba
syifawanda <146319347+syifawanda@users.noreply.github.com>
2023-09-29 20:25:37 +0700

    Add files via upload

---
+++ Week1/Python/fav_movie.py
@@ -0,0 +1,6 @@
+movie = input("What's your favorite movie? ")
+
+if movie == "Avengers Endgame" or movie == "Titanic":
+    print("Good choice!")
+if movie == "Star Wars":
+    print("Also a good choice!")
---
+++ Week1/Python/input2.py
@@ -0,0 +1,5 @@
+name= int(input("What's your name? "))
+
+name = name
+
+print("Nice to meet you %d " % age2)
